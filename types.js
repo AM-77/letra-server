6469cbd8b24f5a1354e798551cc046d7561916da
AM-77 <amine.griche77@gmail.com>
2019-11-23 14:12:46 +0100

    removed search and added search_albums, search_artists and search_tracks

--- types.js
+++ types.js
@@ -3,5 +3,7 @@ module.exports = {
     ARTIST: "artist",
     HOME: "home",
     LIST_ARTISTS: "list-artists",
-    SEARCH: "search"
+    SEARCH_ARTISTS: "search_artists",
+    SEARCH_ALBUMS: "search_albums",
+    SEARCH_TRACKS: "search_tracks",
 }
